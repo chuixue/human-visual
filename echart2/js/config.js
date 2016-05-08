eff25957da2f1d566d9a79482826dbd8988e5707
chuixue simon <chuixue simon>
2016-05-08 17:16:37 +0800

    add echarts

---
+++ echart2/js/config.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+ require.config({
+            packages: [
+                {
+                    name: 'echarts',
+                    location: 'js/echarts/src',
+                    main: 'echarts'
+                },
+                {
+                    name: 'zrender',
+                    location: 'js/zrender/src',
+                    main: 'zrender'
+                }
+            ]
+        });
